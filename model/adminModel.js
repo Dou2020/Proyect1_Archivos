4217a64728125401fe6d4839e6d6952e885359d6
Douglas Gómez <doueduar2018@gmail.com>
2023-10-01 23:59:46 -0600

    add admin

--- model/adminModel.js
+++ model/adminModel.js
@@ -16,5 +16,37 @@ module.exports = {
             dbConnect.connect().end()
         })
         return list
+    },
+    async obtenerEmpleado(usuario){
+        console.log(usuario)
+        let list
+        await dbConnect.connect().query('SELECT * FROM personal.empleado WHERE usuario = $1',[usuario])
+        .then(response => {
+            list = response.rows
+            console.log(list)
+        })
+        .catch(err => {
+            console.log(err)
+            return
+        }).finally( () => {
+            //console.log('cerrar conexion')
+            dbConnect.connect().end()
+        })
+        return list
+    },
+    async actualizarEmpleado(empleado){
+        console.log(empleado)
+        await dbConnect.connect().query('UPDATE personal.empleado SET  password=$1, name=$2, rol=$3, subcursal=$4 WHERE usuario=$5 ',[empleado.password, empleado.name, empleado.rol, empleado.subcursal,empleado.usuario ])
+        .then(response => {
+            //sconsole.log(response.rows)
+        })
+        .catch(err => {
+            console.log(err)
+            return
+        }).finally( () => {
+            //console.log('cerrar conexion')
+            dbConnect.connect().end()
+        })
+        return await this.obtenerEmpleados()
     }
 }
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -15,5 +15,49 @@ module.exports = (router,myCache) => {
             res.render('./Admin/adminIndex',{name:name, opcion:opcion, datas:data});   
         }
     })
+    router.get('/editarEmpleado/:empleado', async(req,res)=> {
+        const name = myCache.get('name') || '';
+        const opcion = req.query.opcion || 1;
+        let data = '';
+        if (name === undefined ) {    
+            res.redirect('/')
+        }
+        if (name.rol == 'adm' && name.estado == 1) {
+            if (opcion == 2) {
+                const empleado = req.params.empleado
+                data = await modelDB.obtenerEmpleado(empleado)
+                console.log(data)
+            }
+            res.render('./Admin/adminIndex',{name:name, opcion:opcion, data:data[0]}); 
+        }
+    })
+    router.post('/UploadEmpleado', async(req,res)=>{
+        const name = myCache.get('name') || '';
+        const opcion =  4;
+        let data = '';
+        if (name === undefined ) {    
+            res.redirect('/')
+        }
+        if (name.rol == 'adm' && name.estado == 1) {
+                const user = req.body.usuario
+                const nam = req.body.name
+                const rol = req.body.rol
+                const sub = req.body.subcursal
+                const password = req.body.password
+
+                //console.log(password,'  ',adminnistrador);
+
+                if ( user !== '' &&  nam !== '' && rol !== '' && sub !== '' && password !== '') {
+                    const cliente = [{usuario:user,name:nam,rol:rol,subcursal:sub,password:password}]
+                    data = await modelDB.actualizarEmpleado(cliente[0]);        
+                }else{
+                    data = await modelDB.obtenerClientes()
+                }
+                //console.log(producto)
+            
+
+            res.render('./Admin/adminIndex',{name:name, opcion:opcion, datas:data});  
+        }
+    })
 
 }
